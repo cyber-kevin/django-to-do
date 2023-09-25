1cef73c67cb9f5d37a7ff605df1dc6d5cdbf8df9
Kevin <devkevferreira@gmail.com>
2023-09-25 10:01:50 -0300

    feat(models): Create the task model and its related migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField(null=True)
+    date = models.DateField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
