7fcbaef3063e61a53a68cc79889ea6bedd0217bc
Kevin <devkevferreira@gmail.com>
2023-10-01 12:11:56 -0300

    feat(views/urls): Create the detail template page, its view and the url associated as well

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from tasks.views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('/<int:task_id>/', detail, name='detail')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from tasks.models import Task
 
@@ -8,3 +8,7 @@ def index(request):
         'latest_tasks_list': latest_tasks_list
     }
     return render(request, 'tasks/index.html', context)
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'tasks/detail.html', {'task': task})
