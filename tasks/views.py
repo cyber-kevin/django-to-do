67b42e71408eb87bb11528d46393551cd28be203
Kevin <devkevferreira@gmail.com>
2023-10-01 12:06:05 -0300

    feat(views/urls): Create the index template page, its view and its url as well

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task
+
+def index(request):
+    latest_tasks_list = Task.objects.order_by('date')[:5]
+    context = {
+        'latest_tasks_list': latest_tasks_list
+    }
+    return render(request, 'tasks/index.html', context)
