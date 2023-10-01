bd4d3aeb426ac65b1f378af1c817a086cbc61792
Kevin <devkevferreira@gmail.com>
2023-10-01 12:21:02 -0300

    feat(views/urls): Create views to handle others CRUD operations for a task and the related urls to these views

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,9 @@ from tasks.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('/add/', add, name='add'),
-    path('/<int:task_id>/', detail, name='detail')
+    path('/<int:task_id>/', detail, name='detail'),
+    path('/uncomplete_task/<int:task_id>/', uncomplete_task, name='uncomplete_task'),
+    path('/complete_task/<int:task_id>/', complete_task, name='complete_task'),
+    path('/edit_body/<int:task_id>/', edit_body, name='edit_body'),
+    path('/delete/<int:task_id>/', delete, name='delete')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from tasks.models import Task
@@ -24,3 +25,38 @@ def add(request):
         task.save()
     
         return redirect('/tasks')
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == 'POST':
+        task.completed = True
+        task.save()
+
+        return redirect('/tasks')
+
+    return HttpResponse('Method not allowed', status=405)
+
+def uncomplete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == 'POST':
+        task.completed = False
+        task.save()
+
+        return redirect('/tasks')
+
+    return HttpResponse('Method not allowed', status=405)
+
+def edit_body(request, task_id):
+    new_body = request.POST[f'task{task_id}']
+
+    task = Task.objects.get(pk=task_id)
+    task.body = new_body
+    task.save()
+
+    return redirect('/tasks')
+
+def delete(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+
+    return redirect('/tasks')
